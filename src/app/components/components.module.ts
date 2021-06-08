b46fb83ccd74629c998298f9818bc0b66ab495ca
Ivan Villacorta <ivanvillacorta@Mac-Ivan.local>
2021-06-08 16:23:32 -0300

    construção do menu lateral

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './template/header/header.component';
+import { SidebarComponent } from './template/sidebar/sidebar.component';
 
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
+    SidebarComponent,
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    HeaderComponent
+    HeaderComponent,
+    SidebarComponent
   ]
 })
 export class ComponentsModule { }
